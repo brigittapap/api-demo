fe292c8514888d06c1603b8f49d37f864e2e3d72
Brigi <pap.mbrigitta@gmail.com>
2021-05-30 15:41:54 +0300

    change root context path

--- src/main/java/com/apidemo/ApidemoApplication.java
+++ src/main/java/com/apidemo/ApidemoApplication.java
@@ -2,9 +2,19 @@ package com.apidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class ApidemoApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Welcome to api-demo application!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApidemoApplication.class, args);
