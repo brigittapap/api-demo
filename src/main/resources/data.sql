6d5b2f196d8c38d17b23314fb9639f5f96175755
Brigi <pap.mbrigitta@gmail.com>
2021-05-31 14:07:40 +0300

    remove unused file

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Note(title, note, datecreated) values('note1', 'note one description', '2021-03-04');
-INSERT INTO Note(title, note, datecreated) values('note2', 'notee twooo', '2020-02-02');
-INSERT INTO Note(title, note, datecreated) values('note2', 'note threee', '2020-01-03');
-
-SELECT * FROM Note
--- INSERT INTO User(name,Note) values ("Rudolf", 1);
-
-COMMIT ;
